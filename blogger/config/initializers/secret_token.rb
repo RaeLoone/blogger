955ac6cfd81857dad4684489b5c679b5237fbe56
Edward Yu <anenomaly@gmail.com>
2014-04-13 15:50:27 -0400

    Added Gem Devise

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'b98d9addfde9eba983d8a7d52327037ee77e685b837e45cf132acc28025ea44ed9dd916a460f332f961f44e25ae73aebf326d8cbfda0c3ae15b22790acb66805'
