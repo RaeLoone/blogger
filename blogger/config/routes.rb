592c5e956d92471d80270d34b6d307ccd273c5cf
Edward Yu <anenomaly@gmail.com>
2014-04-13 16:23:07 -0400

    Added Posts Controller

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,4 +1,8 @@
 Blogger::Application.routes.draw do
+  get "posts/new"
+  get "posts/show"
+  get "posts/update"
+  get "posts/delete"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
