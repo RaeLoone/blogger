9e9bd2e7c3d840d3a0f9b6266b1926163cf0e9ac
Edward Yu <anenomaly@gmail.com>
2014-04-20 10:49:47 -0400

    Created model Category

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415183318) do
+ActiveRecord::Schema.define(version: 20140420144907) do
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
